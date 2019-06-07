f46a6f35d82451831b3bc7b6cdb9ef0e454ab35d
solovev <icemtel@gmail.com>
2019-06-07 11:23:21 +0200

    add friction.py add function to define friction matrix in `triangular_lattice.py`

---
+++ friction.py
@@ -0,0 +1,349 @@
+'''
+Functions to load Fourier series of hydrodynamic friction coefficients from a file
+'''
+import os
+import scipy as sp
+import scipy.linalg as lin
+import scipy.sparse as sparse
+import pandas as pd
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+number_format = '.4G'
+
+
+# Basis functions for one-dimensional Fourier series
+def get_basis_function1D(j):
+    if j > 0:
+        return lambda x: sp.sin(j * x)
+    elif j == 0:
+        return lambda x: 1
+    elif j < 0:
+        return lambda x: sp.cos(j * x)  # cos(-n x) = cos(n x)
+
+
+# Basis functions for two-dimensional Fourier series
+def get_basis_function(j1, j2):
+    return lambda x1, x2: get_basis_function1D(j1)(x1) * get_basis_function1D(j2)(x2)
+
+
+# Compute value of two-dimensional Fourier series, given its list of coefficients
+def fourier_series2D(coeffs, coeff_ids, swap_axes=False):
+    coeffs = sp.array(coeffs)
+    # Save copies of basis functions - I guess this is faster, than calling basis_function(n) each time I want to compute series
+    basis_functions = [get_basis_function(j1, j2) for j1, j2 in coeff_ids]
+
+    if swap_axes:
+        def series(x, y):
+            summ = 0
+            for n, c in enumerate(coeffs):
+                summ += c * basis_functions[n](y, x)
+            return summ
+    else:
+        def series(x, y):
+            summ = 0
+            for n, c in enumerate(coeffs):
+                summ += c * basis_functions[n](x, y)
+            return summ
+
+    return series
+
+
+# Prepare list of mode indices for two-dimensional Fourier series
+def get_coeff_ids(order, sample_size=None, truncate_triangular=False):
+    '''
+    Get ids of the coefficients, get basis function ids up to specified order in each direction.
+    :param order:   Max mode number to extract; if a tuple given - separate number for x and y direction.
+                    Can be `None` - then the sample_size will be used to determine the order.
+    :param sample_size: Tuple (len(xs),len(ys). If given - Sin( N_x x) won't be added to the set, if 2 * N_x = len(xs).
+                        Reason: if xs are equally spaced, starting from 0,
+                        then Sin(N_x x) will evaluate to zero at every sample point.
+    :param truncate_triangular: If True: truncate series, s.t. n_x/N_x + n_y/N_y =< 1
+    '''
+    if sample_size == None:
+        sample_size = (sp.inf, sp.inf)
+    len_xs, len_ys = sample_size
+    if order is None:
+        if len_xs == sp.inf or len_ys == sp.inf:
+            raise ValueError("Either order or sample_size should be specified.")
+        order = len_xs // 2, len_ys // 2
+
+    try:
+        order1, order2 = order  # If order is a tuple
+    except:
+        order1 = order2 = order  # If order is a number
+
+    if 2 * order1 > len_xs or 2 * order2 > len_ys:
+        raise ValueError('Requested more coefficients than number of sample points')
+
+    coeff_ids = []
+    for j1 in range(-order1, order1 + 1):
+        for j2 in range(-order2, order2 + 1):
+            if 2 * j1 == len_xs or 2 * j2 == len_ys:  # Skip Sin(N_x x) or Sin(N_y y)
+                continue
+            elif truncate_triangular and abs(j1) * order2 + abs(j2) * order1 > order1 * order2:
+                continue  # Skip if outside the triangleq
+            else:
+                coeff_ids.append((j1, j2))
+    return coeff_ids
+
+
+def get_translation_rotation_folder(translation, angle, format=number_format):
+    translation_rotation_folder_template = 't={0[0]:{format}}_{0[1]:{format}}_{0[2]} a={1[0]:{format}}_{1[1]:{format}}'
+    return translation_rotation_folder_template.format(translation, angle, format=format)
+
+
+def load_coeffs_from_file(filename, order_max=None, truncate_triangular=False):
+    '''
+    Load Fourier series coefficients from a file.
+    :param order_max: see order in get_coeff_ids()
+    '''
+
+    df = pd.read_csv(filename, dtype={'n1': sp.int32, 'n2': sp.int32, 'coeff': sp.float64})
+    df.set_index(['n1', 'n2'], drop=False, inplace=True)
+
+    if order_max is not None:
+        coeffs_ids_set = get_coeff_ids(order_max, sample_size=None, truncate_triangular=truncate_triangular)
+        ids_in_df = df.index.values
+        # Drop those values, which are not returned by the get_coeff_ids function
+        ids_to_truncate = [idx for idx in ids_in_df if
+                           idx not in coeffs_ids_set]
+        df.drop(index=ids_to_truncate, inplace=True)
+    return df
+
+
+def load_function_from_file(filename, order_max=None, truncate_triangular=False):
+    '''
+    Load Fourier series from a file with coefficients.
+    :param order_max: see order in get_coeff_ids()
+    '''
+    df = load_coeffs_from_file(filename, order_max, truncate_triangular)
+    coeffs = sp.array(df['coeff'])
+    coeff_ids = [(m, n) for (m, n) in zip(df['n1'], df['n2'])]
+
+    series = fourier_series2D(coeffs, coeff_ids)
+    return series
+
+
+###
+# connections = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, -1)]
+
+def load_self_friction_and_interactions(set_name, connections, e1, e2, a, order_g11, order_g12):
+    '''
+    Load as a function of translation?
+    But then I will have troulbe searching for the right entry in the dictionary
+    Load as a function of (n,m)?
+    - Can work, just need to specify e1,e2,a and connection numbers
+    :param set_name: e.g. 'machemer_1'
+    '''
+    if set_name == 'machemer_1':
+        friction_coeffs_root = os.path.join(script_path, 'friction_coeffs', 'machemer_1')
+
+    small_number = 1e-8
+    interactions_dict = {}
+    for n, m in connections:  # relative cilia positions in lattice space
+        translation = a * e1 * n + a * e2 * m
+        # Determine if translation vector is in lower half-plane - then use coeffs from upper part of the plane
+        # but swap cilia indices
+        if translation[1] < 0 or (abs(translation[1]) < small_number and translation[0] < 0):
+            in_lower_halfplane = True
+            translation = - translation
+        else:
+            in_lower_halfplane = False
+
+        # Replace very small coordinates with zero - to correctly compile the folder name
+        for i, ti in enumerate(translation):
+            if abs(ti) < small_number:
+                translation[i] = 0
+
+        # Get file name and load g_ij as a function
+        translation3D = (*translation, 0.)
+        translation_folder = get_translation_rotation_folder(translation3D, (0, 0))
+        filename = os.path.join(friction_coeffs_root, translation_folder, 'g12_ft.dat')
+
+        # Load friction as a function (Fourier sum); swap order of input when needed
+        g_ij = load_function_from_file(filename, order_max=order_g12, truncate_triangular=False)
+
+        df = load_coeffs_from_file(filename, order_max=order_g12, truncate_triangular=False)
+        coeffs = sp.array(df['coeff'])
+        coeff_ids = [(m, n) for (m, n) in zip(df['n1'], df['n2'])]
+
+        # swap variable in fouerier series if in lower half plane - fix of symmetry
+        interactions_dict[(n, m)] = fourier_series2D(coeffs, coeff_ids, swap_axes=in_lower_halfplane)
+
+    ##  Load self-friction
+    self_friction_dict = {}
+    for n, m in connections:
+        translation = a * e1 * n + a * e2 * m
+        # Determine if translation vector is in lower half-plane - then use coeffs from upper part of the plane
+        # but change indexes 1 and 2
+        if translation[1] < 0 or (translation[1] == 0 and translation[0] < 0):
+            in_lower_halfplane = True
+            translation = - translation
+        else:
+            in_lower_halfplane = False
+
+        # Replace very small coordinates with zero - to correctly compile the folder name
+        for i, ti in enumerate(translation):
+            if abs(ti) < 10 ** -8:
+                translation[i] = 0
+
+        translation3D = (*translation, 0.)
+        translation_folder = get_translation_rotation_folder(translation3D, (0, 0))
+
+        if in_lower_halfplane:  # fix symmetry
+            filename = os.path.join(friction_coeffs_root, translation_folder, 'g22_ft.dat')  # TODO: Check
+            order = order_g11[::-1]  # reverse order
+        else:
+            filename = os.path.join(friction_coeffs_root, translation_folder, 'g11_ft.dat')
+            order = order_g11
+
+        # Load friction as a function (fourier sum); swap order of input when needed
+        df = load_coeffs_from_file(filename, order_max=order, truncate_triangular=False)
+        coeffs = sp.array(df['coeff'])
+        coeff_ids = [(m, n) for (m, n) in zip(df['n1'], df['n2'])]
+
+        # swap variable in fouerier series if in lower half plane - fix of symmetry
+        self_friction_dict[(n, m)] = fourier_series2D(coeffs, coeff_ids, swap_axes=in_lower_halfplane)
+
+    return self_friction_dict, interactions_dict
+
+
+# Simple self-friction
+
+
+###
+def define_gmat_glob_and_q_glob0(set_name, connections, e1, e2, a, neighbours_indices, neighbours_rel_positions,
+                                 order_g11, order_g12, period):
+    '''
+    Define friction matrix and active driving force
+
+    Sparse matrix representation (csr)
+    https://docs.scipy.org/docs/scipy/reference/generated/scipy.sparse.csr_matrix.html
+    https://docs.scipy.org/docs/scipy-0.14.0/reference/sparse.linalg.html
+
+    :param neighbours_indices: list of list: first list has indices of the first cilium, second - neighbours of second cilium, etc.
+    :param neighbours_rel_positions: the same structure as `neighbours_indices`; contains relative positions of cilia (taking into account periodicity)
+    '''
+
+    transformation_matrix = lin.inv((sp.array([e1, e2]).T)) / a  # from Euclidean to lattice
+
+    def euclidean_to_lattice_coords(translation):
+        nm = transformation_matrix @ translation
+        return nm
+
+    self_friction_dict, interactions_dict = load_self_friction_and_interactions(set_name, connections,
+                                                                                e1, e2, a, order_g11, order_g12)
+
+    gii = self_friction_dict[(1, 0)]
+
+    rows = []
+    cols = []
+    gij_func_list = []
+
+    for i, (neighbours, translations) in enumerate(zip(neighbours_indices, neighbours_rel_positions)):
+        # Add self-friction
+        rows.append(i)
+        cols.append(i)
+        gij_func_list.append(gii)
+        # Add interactions
+        for j, t in zip(neighbours, translations):
+            nm = euclidean_to_lattice_coords(t)  # Detetermine relative positions of cilia in lattice
+            nm_int = sp.around(nm)
+
+            if not sp.allclose(nm, nm_int, rtol=10 ** -3, atol=10 ** -3):  # Assure correct rounding
+                raise ValueError
+
+            gij = interactions_dict[(nm_int[0], nm_int[1])]
+            rows.append(i)
+            cols.append(j)
+            gij_func_list.append(gij)
+
+    rows = sp.array(rows)
+    cols = sp.array(cols)
+    N = len(neighbours_indices)  # matrix size
+
+    def gmat_glob(phi):
+        '''
+        Compute friction matrix component; return a sparse matrix object.
+        '''
+        vals = []
+        for i, j, gij_func in zip(rows, cols, gij_func_list):
+            vals.append(gij_func(phi[i], phi[j]))
+        return sparse.csr_matrix((vals, (rows, cols)), shape=(N, N))
+
+    freq = 2 * sp.pi / period
+
+    ## Define Q(phi)
+    ## First - calibrate only with self-friction
+    def q_glob(phi):
+        '''
+        :param phi: vector of phases, corresponding to each of the cilia
+        '''
+        return sp.array([freq * gii(phii, 0) for phii in phi])
+
+    return gmat_glob, q_glob
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    set_name = "machemer_1"
+    connections = [(-1, 0), (1, 0)]
+
+    a = 18
+    e1 = sp.array([1, 0])
+    e2 = sp.array([0, 1])
+    order_g11 = (8, 0)
+    order_g12 = (4, 4)
+    T = 31.25
+    N1 = [sp.array([2, 1]), sp.array([0, 2]), sp.array([1, 0])]
+    T1 = [[sp.array([18, 0]), sp.array([-18, 0])],
+          [sp.array([18, 0]), sp.array([-18, 0])],
+          [sp.array([18, 0]), sp.array([-18, 0])]]
+
+    gmat_glob, q_glob = define_gmat_glob_and_q_glob0(set_name, connections, e1, e2, a, order_g11, order_g12, N1, T1, T)
+
+    phis = sp.array([[0, 0, 0], [0.5, 0.5, 0.5], [sp.pi, sp.pi, sp.pi],
+                     [3 / 2 * sp.pi, 3 / 2 * sp.pi, 3 / 2 * sp.pi], [2 * sp.pi, 2 * sp.pi, 2 * sp.pi]])
+
+    plt.plot(phis.T[0], 1 / 2 / sp.pi * T * sp.array([q_glob(phi) for phi in phis]))
+    plt.show()
+## Checks
+# # Ben checked: mtwist solution -> classes of cilia with different inital phase have the same phase after N cycles
+# # Is self-friction different if cilia are located differently? Check
+# # Result: difference ~0.2%
+# phis = sp.linspace(0, 2 * sp.pi, 50, endpoint=False)
+# test_vals = []
+# for key, g_ii in self_friction_dict.items():
+#     gii_vals = [g_ii(phi,0) for phi in phis]
+#     test_vals.append(gii_vals)
+
+# test_vals = sp.array(test_vals)
+# mean_vals = sp.mean(test_vals, axis=0)
+# for i,vals in enumerate(test_vals):
+#     delta = (vals - mean_vals ) / mean_vals
+#     plt.plot(phis, delta)
+# plt.show()
+
+# # Test 2: g_ii(phi,phi); compare with mean_vals obtained earlier
+# test_vals = []
+# for key, g_ii in self_friction_dict.items():
+#     gii_vals = [g_ii(phi,phi) for phi in phis]
+#     test_vals.append(gii_vals)
+
+# test_vals = sp.array(test_vals)
+# for i,vals in enumerate(test_vals):
+#     delta = (vals - mean_vals ) / mean_vals
+#     plt.plot(phis, delta)
+# plt.show()
+
+# # If different - Check one by one
+# for key, g_ii in self_friction_dict.items():
+#     vals = [g_ii(phi,0) for phi in phis]
+#     plt.plot(phis, vals)
+# plt.show()
+
+# for key, g_ii in self_friction_dict.items():
+#     vals = [g_ii(phi,phi) for phi in phis]
+#     plt.plot(phis, vals)
+# plt.show()
---
+++ triangular_lattice.py
@@ -0,0 +1,154 @@
+'''
+Generate triangular lattice
+'''
+import scipy as sp
+from scipy.linalg import norm
+import friction
+
+def get_cell_sizes_triangular(a):
+    cell_length = a
+    cell_height = a * 3 ** (1 / 2) / 2
+    return cell_length, cell_height
+
+def get_basis_triangular():
+    e1 = sp.array([1, 0])
+    e2 = sp.array([0.5, sp.sqrt(3) / 2])
+    return e1,e2
+
+def get_domain_sizes_triangular(nx, ny, a):
+    cell_length, cell_height = get_cell_sizes_triangular(a)
+    L1 = cell_length * nx
+    L2 = cell_height * ny
+    return L1, L2
+
+
+def get_nodes_and_ids(nx, ny, a):
+    '''
+    Create a triangular lattice in rectangular domain
+    :param nx,ny: - Number of cilia in each direction; ny must be even
+    :param a: cilia spacing
+    '''
+    # unit vectors
+    # spatial dimension of simulation domain
+
+    e1 ,e2 = get_basis_triangular()
+
+    cell_length, cell_height = get_cell_sizes_triangular(a)
+    L1, L2 = get_domain_sizes_triangular(nx,ny, a)
+    eps = 10 ** -4 * a  # better safe than sorry: small number to account for rounding errors below
+
+    # generate triangular lattice
+    coords = []  # list of position vectors for honeycomb lattice
+    lattice_ids = []  # list of corresponding lattice indices (n,m)
+
+    for n in range(-4 * nx, 4 * nx):
+        for m in range(0, 4 * ny):
+            x = n * a * e1 + m * a * e2  # position vector
+            # position vector within bounds?
+            if (x[0] >= 0 - eps) and (x[1] >= 0 - eps) and (x[0] < L1 - eps) and (x[1] < L2 - cell_height + eps):
+                coords.append(x)
+                lattice_ids.append((n, m))
+
+    # discard entries not used
+    coords = sp.array(coords)
+    lattice_ids = sp.array(lattice_ids)
+
+    return coords, lattice_ids
+
+
+def get_neighbours_list(coords, nx, ny, a):
+    '''
+    Find nodes which are located at the distance `a`
+    :return: list of neighbours, list of relative neighbour positions
+    '''
+    eps = 10 ** -4 * a
+
+    L1, L2 = get_domain_sizes_triangular(nx, ny, a)
+
+    N = len(coords)
+
+    ## find nearest neigbors
+    def get_neighbours(i, j):
+        '''
+        If the distance between two points is equal to the lattice spacing, return vector connecting them, else None.
+        Takes into account lattice periodicity
+        OK: sign
+        '''
+        for a1 in range(-1, 2):
+            for a2 in range(-1, 2):
+                translation = coords[j, :] - coords[i, :] + [a1 * L1, 0] + [0, a2 * L2]
+                if a - eps < norm(translation) < a + eps:
+                    return translation
+        return None
+
+    ## TO change in case of rectangular domain
+    N1 = [[] for _ in coords]  # list of lists of neighbours
+    T1 = [[] for _ in coords]  # list of translation vectors between neighbours
+    # loop over pairs of lattice points
+    for i in range(N):  # TODO: optimize
+        for j in range(i + 1, N):
+            # lattice points close?
+            # (account for periodic boundary conditions)
+            translation = get_neighbours(i, j)
+            if translation is not None:
+                N1[i].append(j)
+                T1[i].append(translation)
+
+                N1[j].append(i)
+                T1[j].append(- translation)
+    return N1, T1
+
+
+def get_connections():
+    '''
+    :return: Relative positions of neighbouring cilia in lattice coordinates
+    '''
+    return [(-1,0),(1,0),(0,-1),(0,1),(-1,1),(1,-1)]
+
+def define_gmat_glob_and_q_glob_triangular(set_name, a, neighbours_indices, neighbours_rel_positions,
+                                           order_g11, order_g12, T):
+    '''
+    :param set_name: e.g. 'machemer_1'
+    :param a: lattice spacing
+    :param neighbours_indices: list of neighbours
+    :param neighbours_rel_positions: list of relative neighbour positions
+    :param order_g11: Fourier expansion order
+    :param order_g12:
+    :param T:
+    :return: gmat, q_glob  - functions
+    '''
+    connections = get_connections()
+    e1, e2 = get_basis_triangular()
+    return friction.define_gmat_glob_and_q_glob0(set_name, connections, e1, e2, a,
+                                                 neighbours_indices, neighbours_rel_positions,
+                                                 order_g11, order_g12, T)
+
+
+
+
+if __name__ == '__main__':
+    # OK: both triangular and rectangular lattice
+    # Plot:
+    # OK: translations lengths
+    # OK: translations direction
+
+    import visualize
+
+    a = 10
+    nx = 6
+    ny = 6
+
+    coords, lattice_ids = get_nodes_and_ids(nx, ny, a)
+    N1, T1 = get_neighbours_list(coords, nx, ny, a)
+
+    ## Visualize
+    # visualize.plot_edges(coords, T1)
+    # visualize.plot_nodes(coords)
+    # visualize.plt.show()
+
+
+    ## Friction
+    order_g11 = (8,0)
+    order_g12 = (4,4)
+    T = 31.25
+    define_gmat_glob_and_q_glob_triangular('machemer_1', a, N1, T1,order_g11, order_g12,T)
