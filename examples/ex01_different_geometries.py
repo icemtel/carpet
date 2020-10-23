5046e97af66cd6f57f01742cf2e0d85b3ea3c656
icemtel <icemtel@gmail.com>
2020-10-23 12:21:06 +0200

    upd example

--- examples/ex01_different_geometries.py
+++ examples/ex01_different_geometries.py
@@ -61,12 +61,13 @@ a = 18  # [um]
 nx = 3  # number of cilia in x-direction
 ny = 4  # in y-direction:  must be even
 N = nx * ny
+distances = [1]
 
 ## Initialize
 # Geometry
 L1, L2 = lattice.get_domain_sizes(nx, ny, a)
 coords, lattice_ids = lattice.get_nodes_and_ids(nx, ny, a)  # get cilia (nodes) coordinates
-NN, TT = lattice.get_neighbours_list(coords, nx, ny, a)  # get list of neighbours and relative positions
+NN, TT = lattice.get_neighbours_list(coords, nx, ny, a, distances)  # get list of neighbours and relative positions
 e1, e2 = lattice.get_basis()
 get_k = lattice.define_get_k_fbz(nx, ny, a)
 get_mtwist = lattice.define_get_mtwist(coords, nx, ny, a)
@@ -75,6 +76,38 @@ phi = get_mtwist(2, 0)  # sp.zeros([len(coords)])
 vis.plot_edges(coords, TT)
 vis.plot_nodes(coords, phi=phi)
 plt.show()
+
+
+# =====Lattice Triangular 1: special neighbours list=====
+import carpet.lattice.triangular as lattice
+
+# Geometry
+a = 18  # [um]
+nx = 3  # number of cilia in x-direction
+ny = 4  # in y-direction:  must be even
+N = nx * ny
+
+
+connections = [a * np.array([np.cos(psi), np.sin(psi)])  # First neighbours
+               for psi in np.linspace(0, 2 * np.pi, 6, endpoint=False)] \
+              + [np.sqrt(3) * a * np.array([np.cos(psi), np.sin(psi)])
+                  for psi in [np.pi / 2, 3 * np.pi / 2]]  # 2nd neighbour (only 1 direction)
+
+## Initialize
+# Geometry
+L1, L2 = lattice.get_domain_sizes(nx, ny, a)
+coords, lattice_ids = lattice.get_nodes_and_ids(nx, ny, a)  # get cilia (nodes) coordinates
+NN, TT = lattice.get_neighbours_list2(coords, nx, ny, a, connections)  # get list of neighbours and relative positions
+e1, e2 = lattice.get_basis()
+get_k = lattice.define_get_k_fbz(nx, ny, a)
+get_mtwist = lattice.define_get_mtwist(coords, nx, ny, a)
+
+phi = get_mtwist(2, 0)  # sp.zeros([len(coords)])
+vis.plot_edges(coords, TT)
+vis.plot_nodes(coords, phi=phi)
+plt.show()
+
+
 
 # ====Lattice Triangular 2=====
 import carpet.lattice.triangular2 as lattice
