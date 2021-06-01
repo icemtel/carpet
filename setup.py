7a98f4a510d895ccebd822f7f51688f9aa09f025
icemtel <icemtel@gmail.com>
2021-06-01 18:55:09 +0200

    update license, setup examples

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
 setup(name='carpet',
-      #  version='',
-      #  description='',
+       version='2021',
+     #  description='',
       url='https://cfaed.tu-dresden.de/friedrich-home',
-      author='Anton Solovev, Benjamin Friedrich',
-      author_email='anton.solovev@tu-dresden.de',
-      license='Internal Use',
+      author='Anton Solovev, Benjamin M Friedrich',
+      license='MIT',
       packages=['carpet'],
       zip_safe=False)
