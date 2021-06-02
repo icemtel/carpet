29f859180a94b5ed0e8d685dfa754fefd6a5cb82
icemtel <icemtel@gmail.com>
2021-06-02 11:39:34 +0200

    parallel_with_threads.py -> in various

--- carpet/__init__.py
+++ carpet/__init__.py
@@ -1,3 +1,4 @@
 from .dynamics import *
 from .various.math_phase import rms, get_phase_random, order_parameter, get_mean_phase
 from .various.my_logging import setup_logging
+from .various import parallel_with_threads
